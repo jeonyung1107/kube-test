2f5728fa84238f42914d40633844b11ef9632de0
jeon <jeonyng@gmail.com>
2021-04-05 14:57:20 +0900

    init

---
+++ src/main/java/com/caffeinegorilla/kubetest/KubeTestApplication.java
@@ -0,0 +1,13 @@
+package com.caffeinegorilla.kubetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caffeinegorilla/kubetest/KubeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caffeinegorilla.kubetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
